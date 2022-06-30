5fba1a2b0805e72e550e29dbdedff32fddba74dd
EmeditWeb <77761768+EmeditWeb@users.noreply.github.com>
2022-06-30 20:22:20 +0100

    Update Chat engine  API

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import ChatFeed from './component/ChatFeed';
 import LoginForm from './component/LoginForm'
 import './App.css';
 
-export default App;
-import React from 'react';
-import { ChatEngine } from 'react-chat-engine';
-
-export function App() {
-	return (
-		<ChatEngine
-			height='100vh'
-			userName='Emedit'
-			userSecret='iamme'
-			projectID='0f1a093e-1f56-4042-b06b-cf453e31cb6a'
-		/>
-	);
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    return (
+        <ChatEngine 
+        height="100vh" 
+        projectID="0f1a093e-1f56-4042-b06b-cf453e31cb6a"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps} /> }
+        
+        />
+    )
 }
+
+export default App;
